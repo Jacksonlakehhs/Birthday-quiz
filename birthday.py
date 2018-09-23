74f4839b0be39c75f560d25711c9cba9fa161184
Jacksonlakehhs <42844689+Jacksonlakehhs@users.noreply.github.com>
2018-09-23 11:05:18 -0400

    Updated from Brython Server: 9/23/2018 11:05:17 AM

--- birthday.py
+++ birthday.py
@@ -37,11 +37,11 @@ todaymonth = datetime.today().month
 todaydate = datetime.today().day
 month = month_name[todaymonth]
 
-q1 = input("Hello, what is your name?")
+q1 = input("Hello, what is your name? ")
 name = str(q1)
-q2 = input("Hi {0}, what was the name of the month you were born in?".format(name))
-q3 = input("And what year were you born in, {0}?".format(name))
-q4 = input("And the day?")
+q2 = input("Hi {0}, what was the name of the month you were born in? ".format(name))
+q3 = input("And what year were you born in, {0}? ".format(name))
+q4 = input("And the day? ")
 
 q1 = q1.lower()
 q2 = q2.lower()
