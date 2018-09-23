23031e98f0be4fa8c06dcbd3c98e5f36236ad34c
Jacksonlakehhs <42844689+Jacksonlakehhs@users.noreply.github.com>
2018-09-23 12:05:31 -0400

    Updated from Brython Server: 9/23/2018 12:05:30 PM

--- birthday.py
+++ birthday.py
@@ -40,13 +40,11 @@ month = month_name[todaymonth]
 q1 = input("Hello, what is your name? ")
 name = str(q1)
 q2 = input("Hi {0}, what was the name of the month you were born in? ".format(name))
-q3 = input("And what year were you born in, {0}? ".format(name))
-q4 = input("And the day? ")
+q3 = int(input("And what year were you born in, {0}? ".format(name)))
+q4 = int(input("And the day? "))
 
 q1 = q1.lower()
 q2 = q2.lower()
-q3 = q3.lower()
-q4 = q4.lower()
 
 if q2 == "october" and q4 == "31":
     print("You were born on Halloween!")
